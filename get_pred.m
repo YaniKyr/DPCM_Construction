0c0eee85dcc28694a41a9a2345b6a3def4016c8b
YaniKyr <kuriako1234@gmail.com>
2024-01-10 22:14:13 +0200

    test

--- get_pred.m
+++ get_pred.m
@@ -1,15 +1,1 @@
-function y_tide = get_pred(sig,a,p)
-
-    y_tide = zeros(size(sig));
-
-    for n =1:length(sig)
-        if n>p
-            for i =1:p
-                y_tide(n) = y_tide(n) + (a(i)* sig(n-i));
-            end
-        else
-            y_tide(n) = 0;
-    
-        end
-    end
-end
+lol
