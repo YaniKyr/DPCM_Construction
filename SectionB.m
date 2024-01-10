e19a3b62ab0c1d9b059b278780047176288fb4ba
YaniKyr <kuriako1234@gmail.com>
2024-01-10 23:22:03 +0200

    Updated a lot

--- SectionB.m
+++ SectionB.m
@@ -3,38 +3,41 @@ load source.mat
 signal = t;
 p=5;
 
+max_val =3.5;
+min_val = -3.5;
 
-a = get_correlation(signal,p);
+N=8;
+memory= zeros(size(signal));
+y_hat_not = zeros(size(signal));
+y_hat = zeros(size(signal));
+y = zeros(size(signal));
+y_tide = zeros(size(signal));
+for n = 1: length(signal)
+        disp(n)
+     if n>1
+         
+        y(n) = signal(n) - y_tide(n-1);
+     else
+         y(n) = signal(n);
+     end
+    y_hat(n) = my_quantizer(y(n),N,max_val,min_val);
+   
+    if memory == zeros(size(signal))
+        y_hat_not(n) = y_hat(n);
+        memory(n) = y_hat_not(n);
+        y_tide(n) = get_pred(memory,n,p);
 
-y_tide = get_pred(signal,a,p);
-y = zeros(size(y_tide));
-for i =2:length(y_tide)
-    y(i) = signal(i) - y_tide(i-1);
+        
+    else
+       
+        y_hat_not(n) = y_hat(n) +  y_tide(n-1);
+        memory(n) = y_hat_not(n);
+        y_tide(n) = get_pred(memory,n,p);
+        
+    end
+    
 end
-
-
-quantized_err= zeros(size(signal));
-
-for i = 1:size(quantized_err)
-    quantized_err(i) = my_quantizer(y(i),3,3.5,-3.5);
-end
-
 
 
 x = 1:length(signal);
-plot(x,signal,x,quantized_err,Marker=".")
-
-
-abb = get_correlation(quantized_err,p);
-
-x_tide_not = get_pred(quantized_err,abb,p);
-x_tide = zeros(size(x_tide_not));
-for i =2:length(y_tide)
-    x_tide(i) = x_tide_not(i-1) + quantized_err(i);
-end
-
-
-
-sound(signal)
-sound(x_tide)
-plot(x,signal,x,x_tide,Marker=".")
+plot(x,signal,x,y_hat,op);
--- get_pred.m
+++ get_pred.m
@@ -1,1 +1,14 @@
-lol
+function y_tide = get_pred(sig,n,p)
+        y_tide =0;
+        if n>p
+            a = get_correlation(sig,p);
+            
+            for i =1:p
+                y_tide = y_tide + (a(i)* sig(n-i));
+            end
+        else
+            y_tide = 0;
+    
+        end
+    
+end
