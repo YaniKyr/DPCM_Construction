cdc7e25dc300f39977b366b620f1120526981db9
YaniKyr <kuriako1234@gmail.com>
2024-01-14 17:09:39 +0200

    Suppose not a problem

--- untitled2.m
+++ untitled2.m
@@ -1,16 +1,83 @@
+img = imread("parrot.png"); 
+% Read an image
 
-function a = train_predictor (X , p )
-    N = length ( X ) ;
-    R = zeros (p , p ) ;
-    r = zeros (p , 1) ;
-% Compute the elements of R and r
-    for i = 1: p
-        for j = 1: p
-            sum_idx = ( p +1) :( N - max (i , j ) +1) ;
-            R (i , j ) = sum ( X ( sum_idx ) .* X ( sum_idx +i - j ) ) / (N - p ) ;
-        end
-    r ( i ) = sum ( X (( p +1) :( N - i ) ) .* X (( p +1+ i ) : N ) ) / (N - p ) ;
+% Convert the image to a column vector
+img_vector = img(:);
+
+tabulated_data = tabulate(img_vector);
+
+% tabulated data returns counts and propability (%)
+unique_pixel_values = tabulated_data(:, 1);
+pixel_counts = tabulated_data(:, 2);
+pixel_probabilities = pixel_counts / sum(pixel_counts);
+
+%figure;
+%bar(unique_pixel_values, pixel_probabilities);
+%title('Pixel Value Probability Distribution');
+%xlabel('Pixel Value');
+%ylabel('Probability');
+
+%huffenco
+dict = huffmandict(unique_pixel_values,pixel_probabilities);
+
+
+entropy= -sum(pixel_probabilities.*log2(pixel_probabilities));
+
+avglength = sum(cellfun(@length, dict(:,2)) .* pixel_probabilities);
+
+h = entropy/avglength;
+
+
+
+keys = [];
+values = [];
+
+
+%@@@@@@@@@@@ FIX THIS @@@@@@@@@@@@@@@@@@@@
+for i = 1:length(unique_pixel_values)
+    for j = 1:length(unique_pixel_values)
+        newString = strcat(int2str(unique_pixel_values(i)), int2str(unique_pixel_values(j)));
+        keys = [keys, {newString}];
+        values = [values pixel_probabilities(i)* pixel_probabilities(j)];
     end
-    disp(R)
-    a = R \ r ;
 end
+
+tdict = huffmandict(keys,values);
+tentropy= -sum(values.*log2(values));
+
+tavglength = sum(cellfun(@length, tdict(:,2))' .* values);
+
+th = tentropy/tavglength;
+
+%q 4
+enco = huffmanenco(img_vector,dict);
+deco = huffmandeco(enco,dict);
+isequal(img_vector,deco)
+
+bI = reshape((dec2bin(typecast(img_vector,'uint8'),4)-'0').',1,[]);
+J = length(enco)/length(bI);
+
+
+figure;
+imshow(img);
+title('Orginal Image')
+
+DecodedImage = uint8(reshape(deco, [200, 150]));
+figure;
+imshow(DecodedImage);
+title('Decoded Image')
+
+%5
+y = binary_symmetric_channel(enco);
+
+vec = y==enco;
+
+data = tabulate(vec);
+
+counts = data(:, 3);
+p = counts{2} /100;
+
+ChannelSpan = 1-(-p*log2(p)-(1-p)*log2(1-p));
+
+
+%mutual information
